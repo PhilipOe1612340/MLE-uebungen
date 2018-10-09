57d1f66ed01788b212bfdb00bc58b7eba564d00a
Unknown <philip.oesterlin@gmail.com>
2018-10-09 10:10:19 +0200

    p5 setup

---
+++ Woche 2/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+	createCanvas(400, 400);
+}
+
+function draw() {
+	background(220);
+}
+
+
+
+class Rocket {
+	constuctor(byteArr){
+		this.byteArr = byteArr;
+	}
+	
+	let fly = () => {
+		
+	}
+	
+}
