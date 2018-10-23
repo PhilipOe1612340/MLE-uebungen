8124ec04c1f0fbca21095c557d53a26a80545ac2
Unknown <philip.oesterlin@gmail.com>
2018-10-23 12:41:20 +0200

    erklärung für instructions

--- Woche3/src/GeneticAlg/GeneticVM.java
+++ Woche3/src/GeneticAlg/GeneticVM.java
@@ -18,7 +18,22 @@ public class GeneticVM extends VM {
 
     static int[] generateProgram() {
         int[] mem = new int[programLength];
-        mem[0] = 13 << 3;
+
+        // load 0
+        mem[0] = 0;
+
+        /* 
+         * Instruction          
+         * LOAD = 0     000  |  13 bit number
+         * PUSH = 1     001  |  push reg to stack           
+         * POP = 2      010  |  pop from stack into reg           
+         * MUL = 3      011  |  reg * stack[0]           
+         * DIV = 4      100  |  reg / stack[0]           
+         * ADD = 5      101  |  reg + stack[0]           
+         * SUB = 6      110  |  reg - stack[0]           
+         * JIH = 7      111  |  if (reg > 0): set pc forward by stack[0]      
+         */
+
         for (int i = 1; i < mem.length; i++) {
             // int nextInstruction = (int) Math.random() * 8;
             // int bullshit = 13 << 3;
